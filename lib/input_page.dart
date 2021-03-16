a36881c5f677ef3e47b99f9879b6fe2ed31ff3c8
ldasha60 <ldasha60@gmail.com>
2021-03-16 09:50:28 +0700

    Create the layout. Add ReusableCard Widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,17 +10,54 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadius.circular(10.0),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
         ),
-        height: 200.0,
-        width: 170.0,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            )),
+            Expanded(
+              child: ReusableCard(),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(),
+                  ),
+                  Expanded(
+                    child: ReusableCard(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
