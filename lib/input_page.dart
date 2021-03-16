346bbe2703e44eb84a86bf26a98340914b99dd5e
ldasha60 <ldasha60@gmail.com>
2021-03-16 10:41:58 +0700

    Update the constructor. Add Icon and Text

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -21,6 +22,22 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     color: Color(0xFF1D1E33),
+                    cardChild: Column(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                              fontSize: 18.0, color: Color(0xFF8D8E98)),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
@@ -57,15 +74,18 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color});
+  ReusableCard({@required this.color, this.cardChild});
 
   final Color color;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
+        color: color,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
