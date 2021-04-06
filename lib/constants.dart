86abeba0aa955f3ee84b1f2580994cfe45269c58
ldasha60 <ldasha60@gmail.com>
2021-04-06 22:56:34 +0700

    Design results_page.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,3 +13,10 @@ const kNumberTextStyle =
     TextStyle(fontSize: 50.0, color: Colors.white, fontWeight: FontWeight.w900);
 const kButtonTextStyle =
     TextStyle(fontSize: 25.0, color: Colors.white, fontWeight: FontWeight.bold);
+const kTitleTextStyle =
+    TextStyle(fontSize: 50.0, color: Colors.white, fontWeight: FontWeight.bold);
+const kResultTextStyle = TextStyle(
+    fontSize: 22.0, color: Color(0xFF24D876), fontWeight: FontWeight.bold);
+const kBMITextStyle = TextStyle(
+    fontSize: 100.0, color: Colors.white, fontWeight: FontWeight.bold);
+const kBodyTextStyle = TextStyle(fontSize: 22.0, color: Colors.white);
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,17 +1,53 @@
 import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Text(
-        'RESULTS',
-        style: kNumberTextStyle,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Text(
+                  'Your Result',
+                  style: kTitleTextStyle,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+                color: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Normal',
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '18.6',
+                      style: kBMITextStyle,
+                    ),
+                    Text(
+                      'Your BMI is low, you need to eat more!',
+                      style: kBodyTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
